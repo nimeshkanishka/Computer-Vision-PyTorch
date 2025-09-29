67cdc8080ae3e856f7024b8e2001a606f88f3767
Nimesh Kanishka <110521080+nimeshkanishka@users.noreply.github.com>
2025-09-29 10:40:23 +0530

    Change activation function from ReLU to SiLU

--- Doodle-Classifier/network.py
+++ Doodle-Classifier/network.py
@@ -8,21 +8,21 @@ class ClassifierNetwork(nn.Module):
             # Convolutional block 1
             nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1), # [1, 28, 28] -> [32, 28, 28]
             nn.BatchNorm2d(32),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # -> [32, 14, 14]
             nn.Dropout2d(p=0.1),
 
             # Convolutional block 2
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), # -> [64, 14, 14]
             nn.BatchNorm2d(64),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.MaxPool2d(kernel_size=2, stride=2), # -> [64, 7, 7]
             nn.Dropout2d(p=0.1),
 
             # Convolutional block 3
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1), # -> [128, 7, 7]
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.AdaptiveAvgPool2d(output_size=(4, 4)) # -> [128, 4, 4]
         )
 
@@ -30,7 +30,7 @@ class ClassifierNetwork(nn.Module):
             nn.Flatten(),
             nn.Linear(128 * 4 * 4, 256),
             nn.BatchNorm1d(256),
-            nn.ReLU(),
+            nn.SiLU(),
             nn.Dropout(p=0.3),
             nn.Linear(256, 10)
         )
